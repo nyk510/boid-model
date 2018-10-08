9cff82803a0ab3dac60403435de5d67b50a092bb
nyker510 <takamail53@gmail.com>
2018-10-09 00:19:57 +0900

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'portfolio',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'nyker goto portfolio site' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^vuetify/],
+    babel: {
+      plugins: [
+        ['transform-imports', {
+          'vuetify': {
+            'transform': 'vuetify/es5/components/${member}',
+            'preventFullImport': true
+          }
+        }]
+      ]
+    },
+    extractCSS: true,
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, {isDev}) {
+      if (isDev && process.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+      if (process.server) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
+    }
+  }
+}
